1aaa93ad18b09d5fecac88d5dd676067eb14c2a2
Osvaldo Murillo <osv.murillo7@gmail.com>
2023-12-31 12:01:34 -0500

    Initial Commit

---
+++ CoinCleanArchitecture/CoinCleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinCleanArchitectureApp.swift
+//  CoinCleanArchitecture
+//
+//  Created by Raynier Murillo on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinCleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
